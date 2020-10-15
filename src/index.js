eaa3b68902353058bc51f2ed99237a32bdc3df3f
IrinaKorbut <irisharomka96@gmail.com>
2020-10-15 21:13:44 +0300

    implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    for (let j = 0; j < str.length; j++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (
+                stack.length &&
+                str.charAt(j) == bracketsConfig[i][1] &&
+                stack[stack.length - 1] == bracketsConfig[i][0]
+            ) {
+                stack.pop();
+                break;
+            }
+            if (str.charAt(j) == bracketsConfig[i][0]) {
+                stack.push(str.charAt(j));
+                break;
+            }
+            if (str.charAt(j) == bracketsConfig[i][1] && stack.length == 0) {
+                return false;
+            }
+        }
+    }
+    return !stack.length;
+};
